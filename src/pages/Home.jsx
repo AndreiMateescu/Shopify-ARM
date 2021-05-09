afb01627f91e42bfa9d1a58406d7f90b21633761
Andrei Mateescu <rietveld-andreim@netrom.ro>
2021-05-09 16:17:59 +0300

    Display main categories and mock json as back-end

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,43 @@
 import React from "react";
 import Layout from "../components/Layout";
+import HomeCategory from "../components/HomeCategory";
+import products from "../utils/products.json";
 
-function Home() {
-  return (
-    <div>
-      <Layout>
-        <h1>Home</h1>
-      </Layout>
-    </div>
-  );
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    const categories = Object.values(products);
+    this.setState({
+      categories,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Layout>
+          <div className="container">
+            <div className="row">
+              {this.state.categories.map((category, index) => (
+                <HomeCategory
+                  key={index}
+                  image={category.image}
+                  title={category.name}
+                  description={category.description}
+                />
+              ))}
+            </div>
+          </div>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default Home;
